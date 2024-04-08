75b952ccd0dbf72044d3554c92419c2ef3b7af50
Parvina Zulfikorova <parvina@silkroadprofessionals.com>
2024-04-08 10:39:20 +0500

    add data coniguration

---
+++ Infrastructure/Data/Configurations/BuildingConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class BuildingConfiguration : IEntityTypeConfiguration<Building>
+    {
+        public void Configure(EntityTypeBuilder<Building> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            builder.Property(b => b.Address)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(b => b.EntrancesCount)
+                .IsRequired();
+
+            builder.Property(b => b.HasLift)
+                .IsRequired();
+        }
+    }
+}
---
+++ Infrastructure/Data/Configurations/EntranceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class EntranceConfiguration : IEntityTypeConfiguration<Entrance>
+    {
+        public void Configure(EntityTypeBuilder<Entrance> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Number)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.Property(e => e.NumberFloor)
+                .IsRequired();
+
+            builder.HasOne(e => e.Building)
+                .WithMany(b => b.Entrances)
+                .HasForeignKey(e => e.BuildingId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
